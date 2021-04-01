1eb6a331df9b8990e769d4c9d876a4d8085c71b3
Hannah Park <ha3158987@gmail.com>
2021-04-01 17:58:09 +0900

    [TEMPP] commit temporarily for testing

--- builder/src/app.js
+++ builder/src/app.js
@@ -1,14 +1,19 @@
+import util from './js/util.js';
 import tokenizer from './js/tokenizer.js';
 import lexer from './js/lexer.js';
 import parser from './js/parser.js';
 import _ from './js/util.js';
+import isValidate from './js/isValidate.js';
 
+// const jsonParser = (str) = util.pipe(tokenizer, lexer, parser)(str);
 const submitBtn = _.$(".submit");
 const result = {
     tokenizer: _.$(".tokenizer_viewer"),
     lexer: _.$(".lexer_viewer"),
     parser: _.$(".parser_viewer")
 }
+
+// console.log(util.pipe)
 
 submitBtn.addEventListener("click", () => {
     let inputStr = _.$("#input-data").value;
--- builder/src/js/main.js
+++ builder/src/js/main.js
@@ -1,12 +1,12 @@
-//-------------module import------------
-import { pipe } from './util.js';
+import util from './util.js';
 import tokenizer from './tokenizer.js';
 import lexer from './lexer.js';
 import parser from './parser.js';
 import isValidate from './isValidate.js';
 
-//------------json parser를 구동----------
-const JSONParser = str = pipe(tokenizer, lexer, parser)(str);
-const runTestCases = str => JSONParser(str);
+const jsonParser = (str) = util.pipe(tokenizer, lexer, parser)(str);
+console.log(isValidate);
 
-export default runTestCases;
+
+export default jsonParser;
+
